00b4c375ac7ecc3d596843d47cf9be17b72b17fc
g-ruslan <ekb_sl@hotmail.com>
2021-12-25 13:44:31 +0500

    full sample `Database.GetDbConnection()`

---
+++ src/EntityFrameworkCore.Testing.Moq.Tests/FullCaseSample.cs
@@ -0,0 +1,37 @@
+﻿using Apps72.Dev.Data.DbMocker;
+using AutoFixture;
+using EntityFrameworkCore.Testing.Common.Tests;
+using EntityFrameworkCore.Testing.Moq.Helpers;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System.Linq;
+
+namespace EntityFrameworkCore.Testing.Moq.Tests
+{
+    public class FullCaseSample : BaseForTests
+    {
+        [Test]
+        public void Sample()
+        {
+            var entity = Fixture.Create<TestEntity>();
+
+            // external dependency
+            var conn = new MockDbConnection();
+            conn.Mocks.When(w => w.CommandText.Equals("select 5 from test")).ReturnsRow(5);
+
+            var mockedDbContext = new MockedDbContextBuilder<TestDbContext>().UseDbConnection(conn).MockedDbContext;
+            mockedDbContext.TestEntities.Add(entity);
+            mockedDbContext.SaveChanges();
+
+            var connection = mockedDbContext.Database.GetDbConnection();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "select 5 from test";
+                Assert.That(command.ExecuteNonQuery(), Is.EqualTo(5));
+            }
+
+            Assert.NotNull(mockedDbContext.TestEntities.FirstOrDefault());
+        }
+    }
+}
